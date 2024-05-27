20b0cf94d26487aa659882081f965313da299229
Olga <udaltsovaolya@gmail.com>
2024-05-27 16:15:04 +0300

    Из слайса postsSlice удалено состояние allPosts. Вместо него реализована функция getAllPosts для страницы админа.  Из слайса userSlice удален lastUser в связи с ненадобностью.

--- src/redux/slices/postsSlice.js
+++ src/redux/slices/postsSlice.js
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   privatePosts: null,
   publicPosts: null,
-  allPosts: null,
 };
 
 export const postsSlice = createSlice({
@@ -16,18 +15,12 @@ export const postsSlice = createSlice({
       state.privatePosts = state.privatePosts
         ? [newPost, ...state.privatePosts]
         : [newPost];
-      state.allPosts = state.allPosts
-        ? state.allPosts.concat(newPost)
-        : [newPost];
     },
     addToPublic: (state, action) => {
       const newPost = { ...action.payload };
       newPost.id = new Date().getTime();
       state.publicPosts = state.publicPosts
         ? [newPost, ...state.publicPosts]
-        : [newPost];
-      state.allPosts = state.allPosts
-        ? state.allPosts.concat(newPost)
         : [newPost];
     },
     editPost: (state, action) => {
@@ -38,16 +31,8 @@ export const postsSlice = createSlice({
           }
           return post;
         });
-      } else if (state.publicPosts) {
-        state.publicPosts = state.publicPosts.map((post) => {
-          if (post.id === action.payload.id) {
-            return { ...post, post: action.payload.post };
-          }
-          return post;
-        });
       }
-
-      state.allPosts = state.allPosts.map((post) => {
+      state.publicPosts = state.publicPosts.map((post) => {
         if (post.id === action.payload.id) {
           return { ...post, post: action.payload.post };
         }
@@ -59,12 +44,8 @@ export const postsSlice = createSlice({
         state.privatePosts = state.privatePosts.filter(
           (post) => post.id !== action.payload.id
         );
-      } else if (state.publicPosts) {
-        state.publicPosts = state.publicPosts.filter(
-          (post) => post.id !== action.payload.id
-        );
       }
-      state.allPosts = state.allPosts.filter(
+      state.publicPosts = state.publicPosts.filter(
         (post) => post.id !== action.payload.id
       );
     },
@@ -89,20 +70,8 @@ export const postsSlice = createSlice({
           }
           return post;
         });
-      } else if (state.publicPosts) {
-        state.publicPosts = state.publicPosts.map((post) => {
-          if (post.id === comment.post.id) {
-            return {
-              ...post,
-              comments: post.comments
-                ? [...post.comments, newComment]
-                : [newComment],
-            };
-          }
-          return post;
-        });
       }
-      state.allPosts = state.allPosts.map((post) => {
+      state.publicPosts = state.publicPosts.map((post) => {
         if (post.id === comment.post.id) {
           return {
             ...post,
